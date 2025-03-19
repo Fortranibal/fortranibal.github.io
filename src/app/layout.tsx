e84da619a00f39ee2b33c75f1f0120effc2110fb
fortranibal <anibal_g_h@hotmail.com>
2025-03-19 15:04:24 +0100

    udpate: fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,40 +2,26 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from '@/components/theme-provider'
-// import localFont from "next/font/local"
 import { Toaster } from '@/components/ui/toaster'
 
 const inter = Inter({ subsets: ['latin'] })
 
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
-
 export const metadata: Metadata = {
-  metadataBase: new URL ('https://anibalguerrero.com'),
+  metadataBase: new URL('https://anibalguerrero.com'),
   title: {
     default: 'Anibal Guerrero Hernandez Portfolio',
     template: '%s | Anibal Guerrero Hernandez Portfolio',
   },
-  
-  description: 'Portfolio of Anibal Guerrero Hernandez, Aerospace Engineer, Entrepeneur, and Innovator',
+  description: 'Portfolio of Anibal Guerrero Hernandez, Aerospace Engineer, Entrepreneur, and Innovator',
 
   openGraph: {
     title: 'Anibal Guerrero Hernandez Portfolio',
-    description: 'Portfolio of Anibal Guerrero Hernandez, Aerospace Engineer, Entrepeneur, and Innovator',
-    url: '/',
+    description: 'Portfolio of Anibal Guerrero Hernandez, Aerospace Engineer, Entrepreneur, and Innovator',
+    url: 'https://anibalguerrero.com',
     siteName: 'Anibal Guerrero Hernandez Portfolio',
     images: [
       {
-        url: '/public/og_image.png',
+        url: '/og_image.png', // Corrected the image path
         width: 1200,
         height: 630,
         alt: 'Anibal Guerrero Hernandez Portfolio',
@@ -47,8 +33,8 @@ export const metadata: Metadata = {
   twitter: {
     card: 'summary_large_image',
     title: 'Anibal Guerrero Hernandez Portfolio',
-    description: 'Portfolio of Anibal Guerrero Hernandez, Aerospace Engineer, Entrepeneur, and Innovator',
-    images: ['/public/og_image.png'], // Add your image URL
+    description: 'Portfolio of Anibal Guerrero Hernandez, Aerospace Engineer, Entrepreneur, and Innovator',
+    images: ['/og_image.png'], // Corrected the image path
   },
 };
 
@@ -62,9 +48,8 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"   // or 'system'
-          // enableSystem
-          enableSystem={false}  // Prevents light theme automatic override
+          defaultTheme="dark"
+          enableSystem={false} 
           disableTransitionOnChange
         >
           {children}
